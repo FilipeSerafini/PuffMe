ea17f629249739f8f0b49acc49f86313203673b9
Filipe Serafini <filipe.farah@edu.pucrs.br>
2023-09-28 17:13:13 -0300

    making play background sound

--- PuffMe/PuffMe/Utility/SoundManager.swift
+++ PuffMe/PuffMe/Utility/SoundManager.swift
@@ -10,22 +10,46 @@ import Foundation
 import AVFoundation
 
 class SoundManager {
-    static let shared = SoundManager() // Singleton instance
     
-    private var backgroundMusic: SKAudioNode?
+    static let shared = SoundManager()
     
-//    func playBackgroundMusic(filename: String) {
-//        if let music = SKAudioNode(fileNamed: filename) {
-//            backgroundMusic = music
-//            addChild(music)
-//        }
-//    }
-//
-//    func stopBackgroundMusic() {
-//        backgroundMusic?.removeFromParent()
-//    }
-//
-//    func playSoundEffect(filename: String) {
-//        run(SKAction.playSoundFileNamed(filename, waitForCompletion: false))
-//    }
+    private var backgroundMusicPlayer: AVAudioPlayer?
+    private var isBackgroundMusicPaused = false
+    
+    
+    private init() {}
+    
+    func playBackgroundMusic(filename: String) {
+        if let url = Bundle.main.url(forResource: filename, withExtension: "mp3") {
+            do {
+                backgroundMusicPlayer = try AVAudioPlayer(contentsOf: url)
+                backgroundMusicPlayer?.numberOfLoops = -1  // Loop indefinitely
+                backgroundMusicPlayer?.volume = 0.2
+                backgroundMusicPlayer?.prepareToPlay()
+                backgroundMusicPlayer?.play()
+            } catch {
+                print("Error playing background music: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func pauseBackgroundMusic() {
+        if backgroundMusicPlayer?.isPlaying == true {
+            backgroundMusicPlayer?.pause()
+            isBackgroundMusicPaused = true
+        }
+    }
+    
+    func continueBackgroundMusic() {
+        if isBackgroundMusicPaused {
+            backgroundMusicPlayer?.play()
+            isBackgroundMusicPaused = false
+        }
+    }
+    
+    func stopBackgroundMusic() {
+        backgroundMusicPlayer?.stop()
+        isBackgroundMusicPaused = false
+    }
+    
 }

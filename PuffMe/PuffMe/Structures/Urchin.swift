f7c21abd9ac88224ad104ac4d621c50e8120c849
Thiago Parisotto Dias <thiagodias158@hotmail.com>
2023-09-29 16:45:28 -0300

    asset update

--- PuffMe/PuffMe/Structures/Urchin.swift
+++ PuffMe/PuffMe/Structures/Urchin.swift
@@ -9,7 +9,7 @@ import Foundation
 import SpriteKit
 
 class Urchin : Animal {
-    var texture = SKTexture(imageNamed: "Urchin")
+    var texture = SKTexture(imageNamed: "urchin")
     init() {
         let sprite = SKSpriteNode(texture: texture)
         sprite.scale(to: CGSize(width: 50, height: 50))
